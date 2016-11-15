99ed1657b9e464dd538f2b674430f5101442752e
Siddhartha Das <siddhartha.das.me@gmail.com>
2016-11-15 12:21:43 +0530

    First commit

---
+++ TestDiag/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TestDiag.MvcApplication" Language="C#" %>
---
+++ TestDiag/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestDiag.Startup))]
+namespace TestDiag
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
